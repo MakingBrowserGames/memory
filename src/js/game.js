b78e1b6dd7476ae4a858baf969d55d4a179d6fa4
Fernando <framirez@imaginabit.com>
2014-06-05 14:15:09 +0100

    ya se peuden girar las cartas y hacer parejas

--- src/js/game.js
+++ src/js/game.js
@@ -8,9 +8,10 @@
   Game.prototype = {
 
     create: function () {
+      /*
       var x = this.game.width / 2
         , y = this.game.height / 2;
-        /*
+        
       this.player = this.add.sprite(x, y, 'player');
       this.player.anchor.setTo(0.5, 0.5);
       this.input.onDown.add(this.onInputDown, this);
@@ -21,7 +22,9 @@
       this.cards.setAll('input.useHandCursor',true);
 
       //puntuacion
-      this.label_score = this.game.add.text(40, 20, "0", { font: "20px Arial", fill: "#2c3e50"});
+      this.label_score = this.game.add.text(40, 20, '0', { font: '20px Arial', fill: '#2c3e50'});
+      this.score = 0;
+      this.barajar();
 
       this.initCards();      
 
@@ -29,13 +32,14 @@
     },
 
     update: function () {
+      /*
       var x, y, cx, cy, dx, dy, angle, scale;
 
       x = this.input.position.x;
       y = this.input.position.y;
       cx = this.world.centerX;
       cy = this.world.centerY;
-      /*
+      
       angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
       this.player.angle = angle;
 
@@ -56,60 +60,141 @@
       var num= 0;
       //var j = 1;
         // crea una cuadricula de 5x3 cartas 
-        for ( var i = 0 ; i < 5; i++){
+        for ( var i = 0 ; i < 4; i++){
           for ( var j = 0 ; j < 3; j++){
             //console.log( num );
-            this.add_card( i , j, true);
-            num++;
+            this.add_card(num, i , j, true);
+            num++; 
           }
         }
     }, 
-    add_card:  function(x, y, type) {
+    add_card:  function(carta_numero, x, y, type) {
+
 
       var card = this.cards.getFirstExists(false); // ???
-      //selecionar carta al azar
-      // ...
+      //sacar carta de la baraja
+      card.dibujo_carta = this.baraja[carta_numero];
 
       card.anchor.setTo(0.5);
       //card.body.setSize(100,100,0,0); //set size no funciona 
-      card.reset( (x+1)*150 , (y+1)*150 );
+      //lo separamos 80 de la derecha y queda centrado si el ancho es 920
+      card.reset( 80+(x+1)*150 , (y+1)*150 );
       card.scale.setTo(0.1);
-      card.name = "carta "+(x+1)+" "+(y+1);
+      card.name = 'carta '+(x+1)+' '+(y+1);
+      card.tapada = true;
 
-      if (type)
+      if (type){
         this.game.add.tween(card.scale).delay(x*100+1).to({x:1, y:1}, 400).start();
-      else 
+      }
+      else { 
         this.game.add.tween(card.scale).delay(400).to({x:1, y:1}, 400).start();
-
+      }
       //card.events.onInputDown.add(cardClick(card), this);
       card.events.onInputDown.add( this.card_click, this);
       console.log(card.name);
 
     },
     card_click: function(c){
-      var c1 , c2 ,c3 ;
-      console.log("click " + c.name );
-      //console.log(this);
+      /*TODO: 
+        evitar que se pueda picar en la carta si se a acertado
+        evitar que sepueda picar en la misma carta dos veces 
+      */
+      console.log('click ' + c.name );
+      this.n_cartas_levantadas++;
+      if (this.n_cartas_levantadas>2){
+        return; // evita levantar mas de dos cartas 
+      }
+      this.cartas_levantadas.push(c);
+      console.log('n cartas levantadas = '+this.n_cartas_levantadas);
+      console.log('cartas levantadas = '+this.cartas_levantadas.toString());
       //this.game.add.tween(c.scale).delay(100).to({x:0},400).start();
       //this.game.add.tween(c.scale).delay(500).to({x:1},400).start();
       
 
       //c.animations.add('walk');
       //c.animations.play('walk', 50, true);
-      
+      this.girar_carta(c);
+      this.timeCheck = this.time.now;
 
 
-      var girar=this.game.add.tween(c.scale);
+      if (this.n_cartas_levantadas > 1){
+        if ( this.cartas_levantadas[0].dibujo_carta === this.cartas_levantadas[1].dibujo_carta ){
+          console.log('acierto');
+          while(this.cartas_levantadas.length > 0) { this.cartas_levantadas.pop();}
+          this.n_cartas_levantadas = 0;
+          console.log('cartas levantadas:'+this.cartas_levantadas);
+          this.score += 1;
+          this.label_score.text = this.score;
+        } else {
+          console.log('fallo');
+          //virar las cartas boca abajo:
+          //demasiado rapido!! 
+          // las segunda carta se oculta sin poder verse...
+          this.timer = this.game.time.create(this.game);
+          this.timer.add(1000, function(){
+            console.log(this);
+            console.log('cartas levantadas 0 :' + this.cartas_levantadas[0]);
+            this.girar_carta(this.cartas_levantadas[0]);
+            this.girar_carta(this.cartas_levantadas[1]);
+            //console.log(this.timeCheck);  
+            //console.log(this.time.now);
+            console.log('timeee');
+            //vaciar el array 
+            while(this.cartas_levantadas.length > 0) { this.cartas_levantadas.pop();}
+            this.n_cartas_levantadas = 0;
+          }, this);
+          this.timer.start();
+        }        
+      }
+    },
+    girar_carta: function(c){
+      console.log('this :');
+      console.log(this);
+      var girar = this.game.add.tween(c.scale);
       girar.onComplete.add(function(){
-        c.frame += 1;
-      }, this);
-
-      
+        if (c.tapada === true){
+          c.frame = c.dibujo_carta+1; 
+          c.tapada = false;
+        } else {
+          c.frame = 0;
+          c.tapada = true;
+        }
+      }, this);     
 
       girar.to({ x:0 }, 300 )
         .to({ x:1 }, 200)        
         .start();
+
     }
+  };
+  
+  Game.prototype.n_cartas_levantadas = 0;
+  Game.prototype.cartas_levantadas = [];
+  Game.prototype.barajar = function() {
+    var n_cartas = 12; //tiene que ser par 
+    Game.prototype.baraja = [];
+    var baraja = Game.prototype.baraja;
+    var barajaInicial = [];
+
+    for ( var i = 0 ; i < n_cartas/2 ; i++){
+      barajaInicial.push(i);
+    }
+    for ( i = 0 ; i < n_cartas/2 ; i++){
+      barajaInicial.push(i);
+    }
+    // ejemplo si fuera una baraja de 6 cartas ahora habría:
+    // Game.prototype.baraja = [0,1,2,0,1,2];
+    // es decir una baraja con dos cartas de cada tipo
+
+    //ahora es cuando "barajamos" de verdad 
+    for (i=0; i < n_cartas; i++){
+      var randomPosition = this.rnd.integerInRange(0,barajaInicial.length - 1);
+      var thisCarta = barajaInicial[ randomPosition ];
+      baraja.push(thisCarta);
+      //var a = barajaInicial.indexOf(thisCarta);
+      barajaInicial.splice(randomPosition , 1); 
+    }
+    console.log(baraja.toString());    
   };
 
   
